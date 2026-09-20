2341807844ec6e4ce01fecd3afb9df8a0f2a62e1
canadaimmigrationservice <career.ensignenergyservices@gmail.com>
2026-09-20 11:49:55 +0100

    Create ErrorMessage.jsx

---
+++ src/components/ErrorMessage.jsx
@@ -0,0 +1,9 @@
+function ErrorMessage({ message = "Something went wrong. Please try again." }) {
+  return (
+    <div className="alert alert-error" role="alert">
+      {message}
+    </div>
+  );
+}
+
+export default ErrorMessage;
