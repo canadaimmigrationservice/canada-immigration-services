bda2dd13ab10d4018ab4c0212e3011f0c7bf9d9c
canadaimmigrationservice <career.ensignenergyservices@gmail.com>
2026-09-20 22:16:28 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PagePlaceholder from "./components/PagePlaceholder";
+import ProtectedAdminRoute from "./components/ProtectedAdminRoute";
+import AdminLayout from "./layouts/AdminLayout";
+import AuthContextProvider from "./context/AuthContext";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import VisaServicesPage from "./pages/VisaServicesPage";
@@ -9,6 +12,8 @@ import VisaServiceDetailsPage from "./pages/VisaServiceDetailsPage";
 import ApplyPage from "./pages/ApplyPage";
 import CheckApplicationPage from "./pages/CheckApplicationPage";
 import ContactPage from "./pages/ContactPage";
+import AdminLoginPage from "./pages/admin/AdminLoginPage";
+import AdminDashboardPage from "./pages/admin/AdminDashboardPage";
 
 function NotFound() {
   return (
@@ -21,38 +26,37 @@ function NotFound() {
 
 function App() {
   return (
-    <>
+    <AuthContextProvider>
       <Header />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
-
         <Route path="/about" element={<AboutPage />} />
-
-        <Route
-          path="/visa-services"
-          element={<VisaServicesPage />}
-        />
-
+        <Route path="/visa-services" element={<VisaServicesPage />} />
         <Route
           path="/visa-services/:slug"
           element={<VisaServiceDetailsPage />}
         />
-
         <Route path="/apply" element={<ApplyPage />} />
-
         <Route
           path="/check-application"
           element={<CheckApplicationPage />}
         />
-
         <Route path="/contact" element={<ContactPage />} />
+
+        <Route path="/admin/login" element={<AdminLoginPage />} />
+
+        <Route element={<ProtectedAdminRoute />}>
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<AdminDashboardPage />} />
+          </Route>
+        </Route>
 
         <Route path="*" element={<NotFound />} />
       </Routes>
 
       <Footer />
-    </>
+    </AuthContextProvider>
   );
 }
 
